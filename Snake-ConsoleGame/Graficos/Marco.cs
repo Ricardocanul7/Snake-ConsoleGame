76d5db85877db16c52c8c1e202b613d60d3392fe
Ricardo Canul <ricardocanul7@gmail.com>
2018-12-05 23:52:17 -0600

    Cree clase marco para muros donde chocara la serpiente

---
+++ Snake-ConsoleGame/Graficos/Marco.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_ConsoleGame.Graficos
+{
+    class Marco
+    {
+        public int base1 { get; set; }
+        public int altura { get; set; }
+
+        public Marco(int base1, int altura)
+        {
+            this.base1 = base1;
+            this.altura = altura;
+        }
+
+        public void Pintar()
+        {
+            for (int y = 3; y < altura; y++)
+            {
+                Console.SetCursorPosition(0, y);
+                Console.Write("#");
+
+                Console.SetCursorPosition(base1, y);
+                Console.Write("#");
+            }
+
+            for(int x = 0; x < base1; x++)
+            {
+                Console.SetCursorPosition(x, 3);
+                Console.Write("#");
+
+                Console.SetCursorPosition(x, altura);
+                Console.Write("#");
+            }
+        }
+    }
+}
--- Snake-ConsoleGame/Program.cs
+++ Snake-ConsoleGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Snake_ConsoleGame.Graficos;
 
 namespace Snake_ConsoleGame
 {
@@ -13,6 +14,14 @@ namespace Snake_ConsoleGame
             // Colaboradores
             // Alan Alexis Moreno Martinez
             // Ricardo Antoni Canul Flota
+
+
+            // Esto es una prueba
+            Marco marco = new Marco(60, 23);
+            marco.Pintar();
+
+
+            Console.ReadKey();
         }
     }
 }
