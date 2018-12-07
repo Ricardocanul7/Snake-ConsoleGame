1a3183c95337f79b422816b7f516e74a08b2275d
Ricardo Canul <ricardocanul7@gmail.com>
2018-12-06 22:07:13 -0600

    Se mejoró la clase Alimento para poder generar el Alimento aleatoriamente dentro de los margenes del marco

--- Snake-ConsoleGame/Graficos/Alimento.cs
+++ Snake-ConsoleGame/Graficos/Alimento.cs
@@ -8,25 +8,53 @@ namespace Snake_ConsoleGame.Graficos
 {
     class Alimento
     {
-        public Alimento()
+        private int CoordX;
+        private int CoordY;
+        private Marco marco;
+        private Random random;
+
+        public Alimento(Marco marco)
         {
+            this.marco = marco;
+            this.GenerarNuevo();
+        }
+
+        public int getCoordX()
+        {
+            return this.CoordX;
+        }
+
+        public int getCoordY()
+        {
+            return this.CoordY;
+        }
+
+        // Metodo para generar un nuevo alimento en coordenadas nuevas
+        public void GenerarNuevo()
+        {
+            int limiteIzqX = 1; // Limite 1 porque en 0 está impreso el muro del marco
+            int limiteDerX = this.marco.base1 - 1;  // Limite es uno menor que la base porque en la ultima coordenada está impresa el muro
+            int limiteSupY = 4;     // Porque el marco comienza a partir de Y = 3 donde se pinta el muro superior
+            int limiteInfY = this.marco.altura - 1;
+
+            // Genero numeros aleatorios para X y Y dentro de un rango dentro de los muros del marco
+            random = new Random();
+            this.CoordX = random.Next(limiteIzqX, limiteDerX);
+            this.CoordY = random.Next(limiteSupY, limiteInfY);
         }
 
         //Método para imprimir en la coordenada indicada la comida de la serpiente
-        public void PintarComida()//Limites(60,23)
+        public void Pintar()
         {
-            Console.SetCursorPosition(50, 5);
+            Console.SetCursorPosition(this.CoordX, this.CoordY);
             Console.Write("+");
-            Console.SetCursorPosition(6, 9);
-            Console.Write("+");
-            Console.SetCursorPosition(20, 11);
-            Console.Write("+");
-            Console.SetCursorPosition(30, 20);
-            Console.Write("+");
-            Console.SetCursorPosition(45, 22);
-            Console.Write("+");
-            Console.SetCursorPosition(2, 15);
-            Console.Write("+");
+        }
+
+        // Utilizar este metodo cuando se cambie algun valor en los atributos
+        // para refrescar la posicion visual de la comida
+        public void Actualizar()
+        {
+            this.Pintar();
         }
     }
 }
