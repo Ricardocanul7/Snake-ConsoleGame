b34ab34a8e330edd1cd8d7ffd5a824d817a96d46
Ricardo Canul <ricardocanul7@gmail.com>
2018-12-07 00:34:40 -0600

    Se creo clase para el menú principal y se implementó en metodo Main

--- Snake-ConsoleGame/Program.cs
+++ Snake-ConsoleGame/Program.cs
@@ -15,13 +15,54 @@ namespace Snake_ConsoleGame
             // Alan Alexis Moreno Martinez
             // Ricardo Antonio Canul Flota
 
-            // Esto es una prueba
-            Marco marco = new Marco(60, 23);
-            marco.Pintar();
-            Puntuacion puntuacion = new Puntuacion(50, 1);
-            puntuacion.Pintar();
-            Alimento alimento = new Alimento(marco);
-            alimento.Pintar();
+            // 
+            MenuPrincipal menu = new MenuPrincipal(0, 60, 0, 20);
+            menu.Pintar();
+
+            // Objeto para almacenar la tecla que se presione del teclado en cada momento
+            ConsoleKeyInfo tecla;
+
+            do
+            {
+                // Movimiento del cursor para seleccionar opciones
+                do
+                {
+                    tecla = Console.ReadKey();
+                    if (tecla.Key == ConsoleKey.UpArrow)
+                    {
+                        menu.CursorArriba();
+                    }
+                    else
+                    {
+                        if (tecla.Key == ConsoleKey.DownArrow)
+                        {
+                            menu.CursorAbajo();
+                        }
+                    }
+                    menu.Actualizar();
+                } while (tecla.Key != ConsoleKey.Enter);
+
+                // Ejecutar opcion seleccionada
+                switch (menu.GetOpcion())
+                {
+                    case 1: // JUEGO NUEVO
+
+                        break;
+                    case 2: // PUNTUACIONES
+
+                        break;
+                    case 3: // SALIR
+                        Console.SetCursorPosition(0, 23);
+                        Console.Write("Presiona cualquier tecla para salir...");
+                        break;
+                    default:
+                        Console.WriteLine("ERROR! OPCION INCORRECTA!");
+                        break;
+                }
+            } while (menu.GetOpcion() != 3); // Si opcion es 3 entonces se ha seleccionado salir del programa
+            // FIN
+
+
 
             Console.ReadKey();
         }
