a176c5c628e878e98828e558cb1ba0d99312f51a
Ricardo Canul <ricardocanul7@gmail.com>
2018-12-08 13:06:51 -0600

    Se crea clase Jugador como modelo para almacenar punctuacion de cada jugador

---
+++ Snake-ConsoleGame/Modelos/Jugador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_ConsoleGame.Modelos
+{
+    class Jugador
+    {
+        public string Nombre { get; set; }
+        public int Puntuacion { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Jugador()
+        {
+
+        }
+
+        public Jugador(string nombre, int puntuacion, DateTime fecha)
+        {
+            this.Nombre = nombre;
+            this.Puntuacion = puntuacion;
+            this.Fecha = fecha;
+        }
+    }
+}
